fb510b24d556349d25d185250d9da1208daf7d03
AmeyD090 <97330770+AmeyD090@users.noreply.github.com>
2025-03-05 14:56:13 +0530

    Delete SimpleBanking directory

--- SimpleBanking/src/main/java/com/example/banking/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.banking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.banking.model.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
